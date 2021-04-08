e69bfdb96dd82cefee1605509b60383b179c8e9e
Your Name <you@example.com>
2021-04-09 02:46:57 +0600

    first commit

--- load.sh
+++ load.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-service_file=`cat ban.service`
 dir=`pwd`
+sudo apt update
+echo yes | sudo apt-get --fix-missing  install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev
+echo yes | sudo apt-get --fix-missing install python3-pip
 pip3 install -r requirements.txt
-service_file=${service_file//PatH/$dir}
-echo $service_file >> /etc/systemd/system/ban.service
+sed -i -e 's#PatH#$dir#g' ban.service
+cp -r ban.service /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable test.service
-sudo systemctl start test.service
+sudo systemctl enable ban.service
+sudo systemctl start ban.service
