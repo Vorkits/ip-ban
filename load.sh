1ad98c7a303b5469a5997545f34903ac17f05e1f
Your Name <you@example.com>
2021-04-09 01:41:21 +0600

    first commit

--- load.sh
+++ load.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 service_file=`cat ban.service`
 dir=`pwd`
+pip3 install -r requirements.txt
 service_file=${service_file//PatH/$dir}
 echo $service_file >> /etc/systemd/system/ban.service
 sudo systemctl daemon-reload
