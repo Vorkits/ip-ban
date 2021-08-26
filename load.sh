d294e506c0bd206f4386ee41832296888a5c0e48
root <root@sshtounel.sshtounel>
2021-08-26 19:49:52 +0300

    update sed

--- load.sh
+++ load.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-dir=`pwd`
+papath=`pwd`
+echo $papath
+#echo "${BASH_SOURCE[0]}"
+#dirname "${BASH_SOURCE[0]}"
+
 sudo apt update
 echo yes | sudo apt-get --fix-missing  install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev
 echo yes | sudo apt-get --fix-missing install python3-pip
 pip3 install -r requirements.txt
-sed -i -e 's#PatH#$dir#g' ban.service
+sed -i 's|dir|'$papath'|g' ban.service 
 cp -r ban.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable ban.service
